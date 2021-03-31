23849f31172cb16c69225333fe9c45a1b3a4ecde
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2021-03-31 17:33:50 +0200

    Log panics

--- oracle/src/main.rs
+++ oracle/src/main.rs
@@ -1,4 +1,5 @@
-use std::sync::Arc;
+use log::Kind;
+use std::{panic, sync::Arc};
 use tokio::spawn;
 use tracing_subscriber;
 
@@ -13,6 +14,29 @@ mod webserver;
 fn main() {
     tracing_subscriber::fmt::init();
 
+    let log = Arc::new(log::Log::new());
+
+    let log_ = log.clone();
+    panic::set_hook(Box::new(move |info| {
+        let msg = match info.payload().downcast_ref::<&'static str>() {
+            Some(s) => *s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => &**s,
+                None => "Box<Any>",
+            },
+        };
+
+        log_.log(
+            Kind::Error,
+            &match info.location() {
+                Some(location) => {
+                    format!("Panic '{}' at {}:{}", msg, location.file(), location.line())
+                }
+                None => format!("Panic '{}'", msg),
+            },
+        );
+    }));
+
     tokio::runtime::Builder::new()
         .basic_scheduler()
         .enable_all()
@@ -20,7 +44,6 @@ fn main() {
         .unwrap()
         .block_on(async move {
             let state = state::load();
-            let log = Arc::new(log::Log::new());
             let devices = devices::load(state.clone(), log.clone());
             let web_server = spawn(webserver::webserver(
                 devices.clone(),
